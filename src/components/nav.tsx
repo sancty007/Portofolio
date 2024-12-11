a3f9f6af2760ccaf3b50f2948e604928bc75c66b
sancty <sanctifieryawmensah55@gmail.com>
2024-12-11 15:00:01 +0100

    Correction de l'erreur window.location.hash dans nav.tsx

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,28 +1,23 @@
 "use client";
 
 import Link from "next/link";
-import React, { useState } from "react";
+import React from "react";
 import { Button } from "./ui/button";
 import { navSection } from "@/data/navData";
 import handleDownload from "@/pages/api/route";
 
 export default function Nav() {
   // Utilisation de usePathname pour obtenir le chemin d'ancre de la page
-  const [path, setPath] = useState(window.location.hash);
 
   return (
     <nav className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50 hidden md:flex">
       <div className="p-2 gap-2 md:gap-2 flex md:space-x-4 md:flex items-center md:p-4 bg-slate-950/70 bg-opacity-60 backdrop-blur-lg shadow-sm drop-shadow-lg border border-[#3E2ABF]/15 rounded-md text-sm">
-        {Object.entries(navSection).map(([key, label]) => (
+        {Object.entries(navSection).map(([path, label]) => (
           <Link
-            key={key}
-            href={key}
-            className={`${
-              key === path ? "border-b-2 border-[#3E2ABF]" : ""
-            } text-white hover:border-b-2 border-[#3E2ABF] transition-all duration-200 uppercase`}
-            onClick={() => {
-              setPath(key);
-            }}
+            key={path}
+            href={path}
+            className="border-b-2 border-[#3E2ABF]
+          text-white hover:border-b-2 border-[#3E2ABF] transition-all duration-200 uppercase "
           >
             {label}
           </Link>
