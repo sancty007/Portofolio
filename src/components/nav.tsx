2a492f4ca0871ad93739b9ba46b0995708d9f3d5
sancty <sanctifieryawmensah55@gmail.com>
2025-01-08 10:03:20 +0100

    ajout du composant dialog

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -10,8 +10,8 @@ export default function Nav() {
   // Utilisation de usePathname pour obtenir le chemin d'ancre de la page
 
   return (
-    <nav className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50 hidden md:flex">
-      <div className="p-2 gap-2 md:gap-2 flex md:space-x-4 md:flex items-center md:p-4  bg-opacity-60 backdrop-blur-lg shadow-sm drop-shadow-lg border border-[#3E2ABF]/15 rounded-full text-sm">
+    <nav className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50 hidden md:flex ">
+      <div className="p-2 gap-2 md:gap-2 flex md:space-x-4 md:flex items-center md:p-2  bg-opacity-60 backdrop-blur-lg shadow-sm drop-shadow-lg border border-[#3E2ABF]/15 rounded-full text-sm">
         <Navigation />
 
         <Button
